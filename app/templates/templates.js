5a94d42839d569a15625c4d836fa8c27f59920dd
mike <mikeover719@gmail.com>
2015-09-07 21:33:51 -0400

    Add json file and pull template data from it for templates and template details pages

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -12,11 +12,18 @@ angular.module('templateStore.templates', ['ngRoute'])
         });
 }])
 
-.controller('TemplatesCtrl', ['$scope', function($scope) {
-    console.log('TemplatesCtrl Init');
-    console.log($scope);
+.controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get('json/templates.json').success(function(data) {
+        $scope.templates = data;
+    });
 }])
 
-.controller('TemplateDetailsCtrl', ['$scope', function($scope) {
-    
+.controller('TemplateDetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function($scope, $routeParams, $http, $filter) {
+    var templateId = $routeParams.templateId;
+    $http.get('json/templates.json').success(function(data) {
+       $scope.template = $filter('filter')(data, function(d) {
+           return d.id == templateId;
+       })[0];
+    $scope.mainImage = $scope.template.images[0].name;
+    });
 }]);
